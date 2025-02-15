c4076d61481b2023f94b724311b5bd44a4668d14
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-15 15:05:50 +0300

    update MergeSort.java

--- src/SortAlgorithms/MergeSort.java
+++ src/SortAlgorithms/MergeSort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args){
-        int[] a = new int[] {5, 8};
-        int[] b = new int[] {-1, 6};
+        int[] a = new int[] {1, 5};
+        int[] b = new int[] {2, 4};
 
         System.out.println(Arrays.toString(merge(a, b)));
     }
@@ -13,13 +13,13 @@ public class MergeSort {
     public static int[] merge(int[] a, int[] b){
         int[] result = new int[a.length + b.length];
 
-        int i = 0, j = 0, k = 0;
-        while ((i < a.length) && (j < b.length)) {
+        int k = 0, i = 0, j = 0;
+        while (i < a.length && j < b.length) {
             if (a[i] >= b[j]){
                 result[k] = b[j];
                 j++;
             }
-            else if (b[j] >= a[i]){
+            else {
                 result[k] = a[i];
                 i++;
             }
@@ -27,9 +27,17 @@ public class MergeSort {
             k++;
         }
 
-        System.out.println(Arrays.toString(a) + " - a || " + i + " - i");
-        System.out.println(Arrays.toString(b) + " - b || " + j + " - j");
-        System.out.println(Arrays.toString(result));
+        while (i < a.length){
+            result[k] = a[i];
+            k++;
+            i++;
+        }
+
+        while (j < b.length){
+            result[k] = b[j];
+            k++;
+            j++;
+        }
 
         return result;
     }
