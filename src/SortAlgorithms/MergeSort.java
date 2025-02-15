141105aeb5ad7f06f3a76f8f17ff9248e6df68bd
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-15 15:05:50 +0300

    Complete MergeSort.java

--- src/SortAlgorithms/MergeSort.java
+++ src/SortAlgorithms/MergeSort.java
@@ -4,22 +4,34 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args){
-        int[] a = new int[] {5, 8};
-        int[] b = new int[] {-1, 6};
+        int[] n = new int[] {-31, 552, 124, -481, 12, -1, 0, 2235, 1, 3};
 
-        System.out.println(Arrays.toString(merge(a, b)));
+        System.out.println(Arrays.toString(mergeSort(n)));
+    }
+
+    public static int[] mergeSort(int[] array) {
+        if (array.length < 2) {
+            return array;
+        }
+
+        int half = array.length/2;
+
+        int[] a = Arrays.copyOfRange(array, 0, half);
+        int[] b = Arrays.copyOfRange(array, half, array.length);
+
+        return merge(mergeSort(a), mergeSort(b));
     }
 
     public static int[] merge(int[] a, int[] b){
         int[] result = new int[a.length + b.length];
 
-        int i = 0, j = 0, k = 0;
-        while ((i < a.length) && (j < b.length)) {
+        int k = 0, i = 0, j = 0;
+        while (i < a.length && j < b.length) {
             if (a[i] >= b[j]){
                 result[k] = b[j];
                 j++;
             }
-            else if (b[j] >= a[i]){
+            else {
                 result[k] = a[i];
                 i++;
             }
@@ -27,9 +39,17 @@ public class MergeSort {
             k++;
         }
 
-        System.out.println(Arrays.toString(a) + " - a || " + i + " - i");
-        System.out.println(Arrays.toString(b) + " - b || " + j + " - j");
-        System.out.println(Arrays.toString(result));
+        while (i < a.length){
+            result[k] = a[i];
+            k++;
+            i++;
+        }
+
+        while (j < b.length){
+            result[k] = b[j];
+            k++;
+            j++;
+        }
 
         return result;
     }
