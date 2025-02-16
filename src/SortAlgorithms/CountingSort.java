666eda1479dad166713631168d98ffce754c8562
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-16 12:38:39 +0300

    add Documentation CountingSort.java

--- src/SortAlgorithms/CountingSort.java
+++ src/SortAlgorithms/CountingSort.java
@@ -2,6 +2,11 @@ package SortAlgorithms;
 
 import java.util.Arrays;
 
+/**
+ * Counting Sorting is an algorithm without comparison.
+ * The main idea of the algorithm is to calculate how many times each element occurs in the array,
+ * and then fill the source array with the results of this calculation.
+ */
 public class CountingSort {
     public static void main(String[] args) {
         int[] n = new int[] {-31, 552, 124, -481, 12, -1, 0, 2235, 1, 3};
