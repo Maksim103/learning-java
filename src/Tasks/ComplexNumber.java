46ef6205c8714c38320779f1881a3999101e418b
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-20 15:05:28 +0300

    Create ComplexNumber.java

---
+++ src/Tasks/ComplexNumber.java
@@ -0,0 +1,28 @@
+package Tasks;
+
+public final class ComplexNumber {
+    public static void main(String[] args) {
+
+    }
+
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj;
+    }
+}
