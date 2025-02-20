f7fc40ec5f6aee6e51e6e2968853ecd0c6800823
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-20 15:53:55 +0300

    Complete ComplexNumber.java (override equals and hashCode)

--- src/Tasks/ComplexNumber.java
+++ src/Tasks/ComplexNumber.java
@@ -1,8 +1,16 @@
 package Tasks;
 
-public final class ComplexNumber {
+class ComplexNumber {
     public static void main(String[] args) {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(1, 1);
 
+        // a.equals(b) must return true
+        // a.hashCode() must be equal to b.hashCode()
+
+        System.out.println(a.equals(b));
+        System.out.println(a.hashCode() + " - a.hashCode()");
+        System.out.println(b.hashCode() + " - b.hashCode()");
     }
 
     private final double re;
@@ -22,7 +30,27 @@ public final class ComplexNumber {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime*result + (int)Double.doubleToLongBits(getIm());
+        result = prime*result + (int)Double.doubleToLongBits(getRe());
+
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
-        return this == obj;
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != this.getClass() ) {
+            return false;
+        }
+
+        ComplexNumber complexNumber = (ComplexNumber) obj;
+        return Double.compare(complexNumber.im, getIm()) == 0 && Double.compare(complexNumber.re, getRe()) == 0;
     }
 }
