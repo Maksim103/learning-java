67ee4e8354e7dcaa7634a058c95ff9267c3e1dc2
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-25 19:03:11 +0300

    delete main

--- src/FilterComment/Main.java
+++ src/FilterComment/Main.java
@@ -1,10 +1,6 @@
 package FilterComment;
 
 public class Main {
-    public static void main(String[] args) {
-
-    }
-
     public Label checkLabels(TextAnalyzer[] analyzers, String text) {
         return Label.OK;
     }
