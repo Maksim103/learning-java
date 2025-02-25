7bcaa5b866f0ddb800b903a6a56c15d605e3771a
Maksim103 <zigarevmaksim9@gmail.com>
2025-02-25 19:52:59 +0300

    Complete checkLabels method

--- src/FilterComment/Main.java
+++ src/FilterComment/Main.java
@@ -2,6 +2,12 @@ package FilterComment;
 
 public class Main {
     public Label checkLabels(TextAnalyzer[] analyzers, String text) {
+        for (int i = 0; i < analyzers.length; i++) {
+            if (analyzers[i].processText(text) != Label.OK) {
+                return analyzers[i].processText(text);
+            }
+        }
+
         return Label.OK;
     }
 }
