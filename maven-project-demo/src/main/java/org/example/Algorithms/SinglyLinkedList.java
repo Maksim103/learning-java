42052cbba2b80cdec718d8c3613bc8d614308a58
Maksim103 <zigarevmaksim9@gmail.com>
2025-03-31 21:11:33 +0300

    add methods (insertAtStart, insertAtEnd, insertAfterNode)

--- maven-project-demo/src/main/java/org/example/Algorithms/SinglyLinkedList.java
+++ maven-project-demo/src/main/java/org/example/Algorithms/SinglyLinkedList.java
@@ -31,4 +31,36 @@ class Node {
         this.data = data;
         this.next = null;
     }
+
+    public static Node insertAtStart(Node newNode, Node head) {
+        newNode.next = head;
+        head = newNode;
+
+        return head;
+    }
+
+    public static Node insertAtEnd(Node newNode, Node head) {
+        Node currentNode = head;
+
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = newNode;
+        return head;
+    }
+
+    public static Node insertAfterNode(Node newNode, Node head, int index) {
+        Node currentNode = head;
+
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        Node temp = currentNode.next;
+        currentNode.next = newNode;
+        newNode.next = temp;
+
+        return head;
+    }
 }
