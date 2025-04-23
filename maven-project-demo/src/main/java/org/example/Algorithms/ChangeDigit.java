12c62814100db1ef30edb91f0dd56fc7a39112de
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-04-23 18:22:26 +0300

    Create and complete ChangeDigit.java

---
+++ maven-project-demo/src/main/java/org/example/Algorithms/ChangeDigit.java
@@ -0,0 +1,15 @@
+package org.example.Algorithms;
+
+public class ChangeDigit {
+
+    public static void main(String[] args) {
+        System.out.println(changeDigit(422, 2, 9));
+    }
+
+    public static long changeDigit(long num, long k, int newDigit) {
+        k = (long) Math.pow(10, k);
+        double saved = num % k;
+        num = num - (num % (k * 10L));
+        return ((long) (num + (newDigit * k) + saved));
+    }
+}
