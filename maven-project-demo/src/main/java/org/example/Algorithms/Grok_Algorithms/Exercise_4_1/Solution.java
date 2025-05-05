c5e8475649fb9337c1881c9dd74b19941739f324
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-05-05 23:17:46 +0300

    Complete exercise_4_1 from book "Grok_Algorithms"

--- maven-project-demo/src/main/java/org/example/Algorithms/Grok_Algorithms/Exercise_4_1/Solution.java
+++ maven-project-demo/src/main/java/org/example/Algorithms/Grok_Algorithms/Exercise_4_1/Solution.java
@@ -4,20 +4,17 @@ package org.example.Algorithms.Grok_Algorithms.Exercise_4_1;
 Написать рекурсивную функцию sum
 */
 
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
         int[] array = new int[] {3, 5, 2, 6, 1, 3, 5}; // 25
-        System.out.println(sum(array));
+        System.out.println(sum(array, 0));
     }
 
-    public static int sum(int[] array) {
-        if (array.length == 1) {
-            return array[0];
+    public static int sum(int[] array, int index) {
+        if (index == (array.length-1)) {
+            return array[index];
         }
 
-        int[] newArray = Arrays.copyOfRange(array, 1, array.length);
-        return array[0] + sum(newArray);
+        return array[index] + sum(array, index+1);
     }
 }
