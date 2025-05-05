ee4d241b2d1ee49b4090240cd646e4bc9acf7e53
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-05-05 23:37:46 +0300

    Complete exercise_4_2 from book "Grok_Algorithms"

---
+++ maven-project-demo/src/main/java/org/example/Algorithms/Grok_Algorithms/Exercise_4_2/Solution.java
@@ -0,0 +1,20 @@
+package org.example.Algorithms.Grok_Algorithms.Exercise_4_2;
+
+/*
+Написать рекурсивную функцию countingElements
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] array = new int[] {3, 4, 1, 24, 142, 4, 21, 41, 4, 141, 24, 1}; // 12
+        System.out.println(countingElements(array, 0));
+    }
+
+    public static int countingElements(int[] array, int count) {
+        if (count == array.length) {
+            return count;
+        }
+
+        return countingElements(array, count+1);
+    }
+}
