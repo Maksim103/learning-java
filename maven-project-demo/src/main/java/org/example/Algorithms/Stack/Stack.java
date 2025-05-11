78069dd49f7bf4669d4ca1622c248c7b208cf013
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-05-11 16:55:38 +0300

    Created structure Stack

---
+++ maven-project-demo/src/main/java/org/example/Algorithms/Stack/Stack.java
@@ -0,0 +1,52 @@
+package org.example.Algorithms.Stack;
+
+public class Stack {
+    private int size;
+    private long[] stackArray;
+    private int top;
+
+    public Stack(int size) {
+        this.size = size;
+        stackArray = new long[size];
+        top = -1;
+    }
+
+    public void push(long elem) {
+        top++;
+        stackArray[top] = elem;
+    }
+
+    public long pop() {
+        return stackArray[top--];
+    }
+
+    public long peek() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == (size-1));
+    }
+
+    public void printAllElements() {
+        while (top != -1) {
+            long value = stackArray[top--];
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Stack theStack = new Stack(10);
+        theStack.push(20);
+        theStack.push(40);
+        theStack.push(60);
+        theStack.push(80);
+
+        theStack.printAllElements();
+    }
+}
