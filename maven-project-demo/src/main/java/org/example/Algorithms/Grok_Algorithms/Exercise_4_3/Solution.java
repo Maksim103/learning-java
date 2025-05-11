d58d0decca84b13050dd6fa596d61ca35f6c6eeb
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-05-11 16:10:38 +0300

    Complete exercise_4_3 from book "Grok_Algorithms"

---
+++ maven-project-demo/src/main/java/org/example/Algorithms/Grok_Algorithms/Exercise_4_3/Solution.java
@@ -0,0 +1,25 @@
+package org.example.Algorithms.Grok_Algorithms.Exercise_4_3;
+
+/*
+Написать рекурсивную функцию maxElement
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] array = new int[] {3888888, -231, 4342, 11, 6000000, 34, 231, 7777};
+        System.out.println(maxElement(array, 0));
+    }
+
+
+    public static int maxElement(int[] array, int index) {
+        if (index == (array.length-1)) {
+            return array[index];
+        }
+
+        if (array[index] > maxElement(array, index+1)) {
+            return array[index];
+        } else {
+            return maxElement(array, index+1);
+        }
+    }
+}
