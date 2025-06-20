fc0de2595ea55e0545e4afa3e62e676f92f83a5f
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-06-20 14:14:21 +0300

    Add example to Stack

--- maven-project-demo/src/main/java/org/example/AlgorithmsAndStructures/Stack/Stack.java
+++ maven-project-demo/src/main/java/org/example/AlgorithmsAndStructures/Stack/Stack.java
@@ -39,14 +39,4 @@ public class Stack {
         }
         System.out.println();
     }
-
-    public static void main(String[] args) {
-        Stack theStack = new Stack(10);
-        theStack.push(20);
-        theStack.push(40);
-        theStack.push(60);
-        theStack.push(80);
-
-        theStack.printAllElements();
-    }
 }
