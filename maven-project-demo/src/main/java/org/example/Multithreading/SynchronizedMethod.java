2171cd680fdcdd4a5b67081c34a26824dc37cc07
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-08-01 14:26:19 +0300

    Complete Task "Synchronized Method"

---
+++ maven-project-demo/src/main/java/org/example/Multithreading/SynchronizedMethod.java
@@ -0,0 +1,41 @@
+package org.example.Multithreading;
+
+public class SynchronizedMethod {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Job job1 = new Job(sb);
+        Job job2 = new Job(sb);
+
+        Thread thread1 = new Thread(job1);
+        Thread thread2 = new Thread(job2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("Длина строки: " + sb.length());  // Должно быть 20_000
+    }
+}
+
+class Job implements Runnable {
+    private final StringBuilder sb;
+
+    public Job(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10_000; i++) {
+            synchronized (sb) {
+                sb.append("A");
+            }
+        }
+    }
+}
