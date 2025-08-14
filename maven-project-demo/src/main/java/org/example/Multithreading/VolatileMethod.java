3eae491e7e2cc4f9fd8e0c9a436795c043d43ccb
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-08-14 16:50:27 +0300

    Complete Task "Volatile Method"

---
+++ maven-project-demo/src/main/java/org/example/Multithreading/VolatileMethod.java
@@ -0,0 +1,27 @@
+package org.example.Multithreading;
+
+public class VolatileMethod {
+    private volatile static boolean RUNNING = true;
+
+    public static class Counter implements Runnable {
+        @Override
+        public void run() {
+            long count = 0;
+            while (RUNNING) {
+                count++;
+            }
+
+            System.out.println(count); // не выведется, если не добавить к переменной RUNNING слово volatile
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start");
+
+        new Thread(new Counter(), "Counter").start();
+
+        Thread.sleep(10_000);
+        RUNNING = false;
+        System.out.println("Stop");
+    }
+}
