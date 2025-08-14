8fd8e8540fabd4b25ea739370b6274a2a706df2e
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2025-08-14 13:57:21 +0300

    Complete Task "Interrupt Practice"

---
+++ maven-project-demo/src/main/java/org/example/Multithreading/InterruptPractice.java
@@ -0,0 +1,40 @@
+package org.example.Multithreading;
+
+public class InterruptPractice {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Counter(), "Counter");
+        thread.start();
+
+        Thread.sleep(5_000);
+        thread.interrupt();
+    }
+}
+
+class Counter implements Runnable {
+    private long count = 0;
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            count++;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().isInterrupted()); // После исключения флаг interrupt сбрасывается на false
+
+                Thread.currentThread().interrupt();
+                System.out.println(Thread.interrupted()); // Сразу выводит текущий флаг interrupt сбрасывается на false
+                System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().isInterrupted());
+
+                Thread.currentThread().interrupt();
+                System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().isInterrupted());
+
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
